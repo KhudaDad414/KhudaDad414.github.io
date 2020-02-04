d9ff20a386eca4f11269e070a8d35d6933ec9371
khudadad414 <khudadad414@gmail.com>
2020-02-04 13:56:38 +0530

    First commit

---
+++ precache-manifest.247279e368a181af6408452f5dec2093.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46d06c36c6101338db92de61e5485555",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc6e8654047a28f37ea7",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "355f9eb5b1121fa67443",
+    "url": "/static/js/2.69c5d63b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.69c5d63b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc6e8654047a28f37ea7",
+    "url": "/static/js/main.a8f826c2.chunk.js"
+  },
+  {
+    "revision": "92a8fa6d2a61009e0c1c",
+    "url": "/static/js/runtime-main.e3441cc1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
